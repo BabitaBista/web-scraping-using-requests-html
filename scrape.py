71a45a5b75282f1dbc868aa8af3f8816fe514240
BabitaBista <bistababita903@gmail.com>
2023-07-30 23:14:23 +0545

    second commit

--- scrape.py
+++ scrape.py
@@ -1,19 +1,30 @@
+import csv
 from requests_html import HTMLSession
+
+csv_file = open('AI_health_scrape.csv', 'w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['headline', 'summary', 'video'])
 
 session = HTMLSession()
 res = session.get('https://health.google/health-research/imaging-and-diagnostics/?fbclid=IwAR3JE5J3b5gsbVXgV_UyoOPm780yCUCcNigDDmBnDSTz4eTCzFcKEk6DNnU')
 
+# get all the absolute path for the link in the given page
+# for link in res.html.absolute_links:
+#     print(link)
+
 articles = res.html.find('.glue-grid')
-print(articles)
+# print(articles)
 
 for article in articles:
     try:
         headline = article.find('.glue-headline', first=True).text
         summary = article.find('.g-h-content-block__text p', first=True).text
+        video = article.find('video source', first=True).attrs['src']
 
     except Exception as e:
         headline = None
         summary = None
+        video = None
 
-    print(headline)
-    print(summary)
+csv_writer.writerow([headline, summary, video])
+csv_file.close()
